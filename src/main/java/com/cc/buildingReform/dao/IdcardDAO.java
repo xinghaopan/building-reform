6c37d81a3b728b88550385d7fa47ab1d33e27e48
pan <1652956255@qq.com>
2016-11-07 22:01:08 +0800

    身份证校验时，只验证身份证号，不验证id

--- src/main/java/com/cc/buildingReform/dao/IdcardDAO.java
+++ src/main/java/com/cc/buildingReform/dao/IdcardDAO.java
@@ -18,10 +18,11 @@ public class IdcardDAO extends CcHibernateDao<Idcard, Integer> {
 	@SuppressWarnings("unchecked")
 	public int checkId(Integer id, String idcard) {
 		Criteria criteria = getSession().createCriteria(Idcard.class);
-		
-		if (id != null) {
-			criteria.add(Restrictions.ne("id", id));
-		}
+
+		// 身份证号校验，不需要判断id，只要判断这个号码是否存在
+//		if (id != null) {
+//			criteria.add(Restrictions.ne("id", id));
+//		}
 		criteria.add(Restrictions.eq("idcard", idcard.toUpperCase()));
 		
 		List<Idcard> list = (List<Idcard>) criteria.list();
